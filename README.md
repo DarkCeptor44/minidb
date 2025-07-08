de291a2b3085732c3ba420a220ba0d0900d11e01
DarkCeptor44 <16278483+DarkCeptor44@users.noreply.github.com>
2025-07-08 14:14:02 -0300

    minidb: feat: initial database client impl

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # MiniDB
 
 Minimalistic file-based database written in Rust
+
+## Features
+
+* File-based
+* Thread-safe due to [`parking_lot's`](https://docs.rs/parking_lot) `RwLock`
--- crates/minidb/src/lib.rs
+++ crates/minidb/src/lib.rs
@@ -1,14 +1,119 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+//! # minidb
+//!
+//! Minimalistic file-based database written in Rust
+//!
+//! ## Features
+//!
+//! * File-based
+//! * Thread-safe due to [`parking_lot's`](parking_lot) [`RwLock`]
+
+// Copyright (c) 2025, DarkCeptor44
+//
+// This file is licensed under the GNU Lesser General Public License
+// (either version 3 or, at your option, any later version).
+//
+// This software comes without any warranty, express or implied. See the
+// GNU Lesser General Public License for details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this software. If not, see <https://www.gnu.org/licenses/>.
+
+#![forbid(unsafe_code)]
+#![warn(clippy::pedantic, missing_docs, missing_debug_implementations)]
+
+use std::{
+    collections::HashSet,
+    path::{Path, PathBuf},
+    sync::Arc,
+};
+
+use anyhow::Result;
+use parking_lot::RwLock;
+
+/// Database client
+#[derive(Debug, Clone)]
+pub struct Database {
+    path: Arc<RwLock<PathBuf>>,
+    file_lock: Arc<RwLock<()>>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Database {
+    /// Creates a new database builder
+    #[must_use]
+    pub fn builder() -> DatabaseBuilder {
+        DatabaseBuilder::default()
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    // ----------------------
+    // END OF BUILDER METHODS
+    // ----------------------
+}
+
+/// A builder for [Database]
+#[derive(Debug, Default)]
+pub struct DatabaseBuilder {
+    path: Option<PathBuf>,
+    tables: HashSet<String>,
+}
+
+impl DatabaseBuilder {
+    /// Creates a new database builder
+    ///
+    /// ## Arguments
+    ///
+    /// * `path` - The path to the database
+    ///
+    /// ## Returns
+    ///
+    /// A new [`DatabaseBuilder`]
+    pub fn new<P>(path: P) -> Self
+    where
+        P: AsRef<Path>,
+    {
+        let path = path.as_ref();
+
+        Self {
+            path: Some(path.to_path_buf()),
+            tables: HashSet::new(),
+        }
+    }
+
+    // ------------------------
+    // START OF BUILDER METHODS
+    // ------------------------
+
+    /// Sets the database path
+    ///
+    /// ## Arguments
+    ///
+    /// * `path` - The path to the database
+    #[must_use]
+    pub fn path<P>(mut self, path: P) -> Self
+    where
+        P: AsRef<Path>,
+    {
+        let path = path.as_ref();
+
+        self.path = Some(path.to_path_buf());
+        self
+    }
+
+    /// Adds a table to the database
+    #[must_use]
+    pub fn table<T>(mut self) -> Self {
+        todo!()
+    }
+
+    // ----------------------
+    // END OF BUILDER METHODS
+    // ----------------------
+
+    /// Builds the database
+    ///
+    /// ## Returns
+    ///
+    /// A database client
+    pub fn build(self) -> Result<Database> {
+        todo!()
     }
 }

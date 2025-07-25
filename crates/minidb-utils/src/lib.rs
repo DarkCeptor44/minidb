9be5d0010fb4546b8354325f7730677bc4c3d3aa
DarkCeptor44 <16278483+DarkCeptor44@users.noreply.github.com>
2025-07-25 17:28:55 -0300

    minidb-utils: chore: add license header

--- crates/minidb-utils/src/lib.rs
+++ crates/minidb-utils/src/lib.rs
@@ -2,3 +2,14 @@
 
 #![forbid(unsafe_code)]
 #![warn(clippy::pedantic, missing_debug_implementations, missing_docs)]
+
+// Copyright (c) 2025, DarkCeptor44
+//
+// This file is licensed under the GNU Lesser General Public License
+// (either version 3 or, at your option, any later version).
+//
+// This software comes without any warranty, express or implied. See the
+// GNU Lesser General Public License for details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this software. If not, see <https://www.gnu.org/licenses/>.

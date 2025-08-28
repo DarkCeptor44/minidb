fab516d786a126a39dc4f318e4500c480638c8cf
DarkCeptor44 <16278483+DarkCeptor44@users.noreply.github.com>
2025-08-28 14:59:08 -0300

    minidb-utils: test: add more tests for pathext

--- minidb-utils/tests/path.rs
+++ minidb-utils/tests/path.rs
@@ -25,4 +25,31 @@ fn test_pathext_is_empty() {
     );
 }
 
-// TODO add more tests
+#[test]
+fn test_pathext_is_empty_for_file() {
+    let temp_dir = tempdir().expect("Failed to create temporary directory");
+    let file_path = temp_dir.path().join("file.txt");
+    let mut file = File::create(&file_path).expect("Failed to create file");
+    file.write_all(b"Hello world")
+        .expect("Failed to write to file");
+
+    assert!(
+        !file_path
+            .is_empty()
+            .expect("Failed to check if path is empty"),
+        "File path should not be considered an empty directory"
+    );
+}
+
+#[test]
+fn test_pathext_is_empty_for_non_existent_path() {
+    let temp_dir = tempdir().expect("Failed to create temporary directory");
+    let non_existent_path = temp_dir.path().join("non_existent");
+
+    assert!(
+        !non_existent_path
+            .is_empty()
+            .expect("is_empty should not fail for non-existent path"),
+        "Non-existent path should not be considered an empty directory"
+    );
+}

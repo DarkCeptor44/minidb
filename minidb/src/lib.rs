d7c55d2a901c4d1415e903ee560a4ff23fd2efa9
DarkCeptor44 <16278483+DarkCeptor44@users.noreply.github.com>
2026-01-26 23:47:26 -0300

    minidb: feat: add extra methods for store

--- minidb/src/lib.rs
+++ minidb/src/lib.rs
@@ -1,8 +1,12 @@
+pub use redb;
+
 use std::path::Path;
 
 use anyhow::{Context, Result};
 use redb::{Database, ReadableDatabase, ReadableTable, TableDefinition};
 use serde::{Deserialize, Serialize};
+
+const SETTINGS_TABLE: TableDefinition<&'static str, &[u8]> = TableDefinition::new("settings");
 
 pub trait TableModel: Serialize + for<'de> Deserialize<'de> {
     const TABLE: TableDefinition<'_, &'static str, &[u8]>;
@@ -23,7 +27,105 @@ impl Store {
         let db = Database::builder()
             .create(path)
             .context("failed to create database")?;
-        Ok(Self { db })
+        let store = Self { db };
+        store
+            .create_table_impl(SETTINGS_TABLE)
+            .context("failed to bootstrap tables")?;
+
+        Ok(store)
+    }
+
+    // EMD OF BUILDERS
+
+    pub fn all<T>(&self) -> Result<Vec<T>>
+    where
+        T: TableModel,
+    {
+        let txn = self.db.begin_read().context("failed to begin read")?;
+        let table = txn.open_table(T::TABLE).context("failed to open table")?;
+
+        let mut results = Vec::new();
+        for item in table.iter()? {
+            let (_key, value) = item?;
+            let decoded: T = postcard::from_bytes(value.value())
+                .context("failed to deserialize from postcard")?;
+            results.push(decoded);
+        }
+
+        Ok(results)
+    }
+
+    pub fn create_table<T>(&self) -> Result<()>
+    where
+        T: TableModel,
+    {
+        self.create_table_impl(T::TABLE)
+    }
+
+    fn create_table_impl<K, V>(&self, table: TableDefinition<K, V>) -> Result<()>
+    where
+        K: redb::Key,
+        V: redb::Value,
+    {
+        let txn = self.db.begin_write().context("failed to begin write")?;
+        {
+            let _ = txn.open_table(table).context("failed to open table")?;
+        }
+        txn.commit().context("failed to commit write")?;
+        Ok(())
+    }
+
+    pub fn for_each<T, F>(&self, mut f: F) -> Result<()>
+    where
+        T: TableModel,
+        F: FnMut(T),
+    {
+        let txn = self.db.begin_read().context("failed to begin read")?;
+        let table = txn.open_table(T::TABLE)?;
+
+        for item in table.iter()? {
+            let (_, value) = item?;
+            let data: T = postcard::from_bytes(value.value())?;
+            f(data);
+        }
+
+        Ok(())
+    }
+
+    pub fn get<T>(&self, id: &str) -> Result<Option<T>>
+    where
+        T: TableModel,
+    {
+        let txn = self.db.begin_read().context("failed to begin read")?;
+        let table = txn.open_table(T::TABLE).context("failed to open table")?;
+        let value = table.get(id).context("failed to get item from store")?;
+
+        if let Some(bytes) = value {
+            let item: T = postcard::from_bytes(bytes.value())
+                .context("failed to deserialize from postcard")?;
+            Ok(Some(item))
+        } else {
+            Ok(None)
+        }
+    }
+
+    pub fn get_setting<T>(&self, key: &str) -> Result<Option<T>>
+    where
+        T: for<'a> Deserialize<'a>,
+    {
+        let txn = self.db.begin_read().context("failed to begin read")?;
+        let table = txn
+            .open_table(SETTINGS_TABLE)
+            .context("failed to open table")?;
+        let value = table.get(key).context("failed to get item from store")?;
+
+        if let Some(bytes) = value {
+            let item: T = postcard::from_bytes(bytes.value())
+                .context("failed to deserialize from postcard")?;
+            Ok(Some(item))
+        } else {
+            Ok(None)
+        }
     }
 
     pub fn save<T>(&self, mut item: T) -> Result<()>
@@ -47,38 +149,21 @@ impl Store {
         Ok(())
     }
 
-    pub fn get<T>(&self, id: &str) -> Result<Option<T>>
+    pub fn set_setting<T>(&self, key: &str, value: &T) -> Result<()>
     where
-        T: TableModel,
+        T: Serialize,
     {
-        let txn = self.db.begin_read().context("failed to begin read")?;
-        let table = txn.open_table(T::TABLE).context("failed to open table")?;
-        let value = table.get(id).context("failed to get item from store")?;
-
-        if let Some(bytes) = value {
-            let item: T = postcard::from_bytes(bytes.value())
-                .context("failed to deserialize from postcard")?;
-            Ok(Some(item))
-        } else {
-            Ok(None)
+        let txn = self.db.begin_write().context("failed to begin write")?;
+        {
+            let mut table = txn
+                .open_table(SETTINGS_TABLE)
+                .context("failed to open table")?;
+            let bytes = postcard::to_stdvec(value).context("failed to serialize to postcard")?;
+            table
+                .insert(key, bytes.as_slice())
+                .context("failed to insert into table")?;
         }
-    }
-
-    pub fn all<T>(&self) -> Result<Vec<T>>
-    where
-        T: TableModel,
-    {
-        let txn = self.db.begin_read().context("failed to begin read")?;
-        let table = txn.open_table(T::TABLE).context("failed to open table")?;
-
-        let mut results = Vec::new();
-        for item in table.iter()? {
-            let (_key, value) = item?;
-            let decoded: T = postcard::from_bytes(value.value())
-                .context("failed to deserialize from postcard")?;
-            results.push(decoded);
-        }
-
-        Ok(results)
+        txn.commit().context("failed to commit to database")?;
+        Ok(())
     }
 }

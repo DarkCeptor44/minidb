517181b298f5330f2825ba2f1cc6b56d53141db0
DarkCeptor44 <16278483+DarkCeptor44@users.noreply.github.com>
2026-02-11 20:21:54 -0300

    minidb: doc: add rustdocs for most functions and methods

--- minidb/src/lib.rs
+++ minidb/src/lib.rs
@@ -24,6 +24,33 @@ const META_KEY_SALT: &str = "salt";
 type Initializer = Box<dyn Fn(&WriteTransaction) -> Result<()>>;
 type ArgonKey = [u8; 32];
 
+/// A table model. A table model is a struct that implements the [`TableModel`] trait.
+///
+/// ## Example
+///
+/// ```rust,no_run
+/// use minidb::TableModel;
+/// use serde::{Deserialize, Serialize};
+///
+/// #[derive(Serialize, Deserialize)]
+/// struct Person {
+///     id: String,
+///     name: String,
+///     age: u8,
+/// }
+///
+/// impl TableModel for Person {
+///     const TABLE: redb::TableDefinition<'_, &'static str, &[u8]> = redb::TableDefinition::new("people");
+///
+///     fn get_id(&self) -> &str {
+///         &self.id
+///     }
+///
+///     fn set_id(&mut self, id: String) {
+///         self.id = id;
+///     }
+/// }
+/// ```
 pub trait TableModel: Serialize + for<'de> Deserialize<'de> {
     const TABLE: TableDefinition<'_, &'static str, &[u8]>;
 
@@ -31,6 +58,22 @@ pub trait TableModel: Serialize + for<'de> Deserialize<'de> {
     fn set_id(&mut self, id: String);
 }
 
+/// A builder for a [`MiniDB`]
+///
+/// ## Example
+///
+/// ```rust,ignore
+/// use minidb::MiniDBBuilder;
+///
+/// // create the database with the path to the database file. The file can have any extension
+/// // but it's recommended to use `.redb` so you can differentiate
+/// // between a MiniDB/redb database and a SQLite/other embedded database
+/// let db = MiniDBBuilder::new("test.redb")
+///     .table::<Person>() // you must register all table models
+///     .table::<Car>()
+///     .build()
+///     .unwrap();
+/// ```
 pub struct MiniDBBuilder {
     path: PathBuf,
     initializers: Vec<Initializer>,
@@ -38,6 +81,24 @@ pub struct MiniDBBuilder {
 }
 
 impl MiniDBBuilder {
+    /// Creates a new [`MiniDBBuilder`]
+    ///
+    /// ## Arguments
+    ///
+    /// * `path` - The path to the database file
+    ///
+    /// ## Returns
+    ///
+    /// A new [`MiniDBBuilder`]
+    ///
+    /// ## Example
+    ///
+    /// ```rust,no_run
+    /// use minidb::MiniDB;
+    ///
+    /// // create a MiniDB builder with the file path
+    /// let db = MiniDB::builder("test.redb");
+    /// ```
     pub fn new<P>(path: P) -> Self
     where
         P: Into<PathBuf>,
@@ -49,6 +110,32 @@ impl MiniDBBuilder {
         }
     }
 
+    /// Registers a table model
+    ///
+    /// ## Arguments
+    ///
+    /// * `T` - The table model to register
+    ///
+    /// ## Returns
+    ///
+    /// A new [`MiniDBBuilder`]
+    ///
+    /// ## Example
+    ///
+    /// ```rust,ignore
+    /// use minidb::{MiniDB, Table};
+    ///
+    /// #[derive(Table)]
+    /// struct Person{
+    ///     #[key]
+    ///     id: String,
+    /// }
+    ///
+    /// // create a MiniDB builder with the file path
+    /// // and register the table Person
+    /// let db = MiniDB::builder("test.redb")
+    ///     .table::<Person>();
+    /// ```
     pub fn table<T>(mut self) -> Self
     where
         T: TableModel + 'static,
@@ -62,11 +149,68 @@ impl MiniDBBuilder {
         self
     }
 
+    /// Sets the key source
+    ///
+    /// ## Arguments
+    ///
+    /// * `source` - The key source, can be a password, a pre-derived key, or a function that returns a key (`[u8; 32]`)
+    ///
+    /// ## Returns
+    ///
+    /// A new [`MiniDBBuilder`]
+    ///
+    /// ## Example
+    ///
+    /// ```rust,no_run
+    /// use minidb::{KeySource, MiniDB};
+    ///
+    /// // create a MiniDB builder with a password
+    /// let db = MiniDB::builder("test.redb")
+    ///     // skipping table registering for convenience
+    ///     .key_source(KeySource::Password("secretpassword".to_string()));
+    ///
+    /// // create a MiniDB builder with a pre-derived key
+    /// let key = [1u8; 32];
+    /// let db = MiniDB::builder("test.redb")
+    ///     // skipping table registering for convenience
+    ///     .key_source(KeySource::PreDerived(key));
+    ///
+    /// // create a MiniDB builder with a function that returns a key
+    /// fn key_provider() -> [u8; 32] {
+    ///     [1u8; 32]
+    /// }
+    ///
+    /// let db = MiniDB::builder("test.redb")
+    ///     // skipping table registering for convenience
+    ///     .key_source(KeySource::ExternalKeyProvider(Box::new(key_provider)));
+    /// ```
     pub fn key_source(mut self, source: KeySource) -> Self {
         self.key_source = Some(source);
         self
     }
 
+    /// Builds the [`MiniDB`] from the builder
+    ///
+    /// ## Returns
+    ///
+    /// A new [`MiniDB`]
+    ///
+    /// ## Errors
+    ///
+    /// Returns an error if the database file already exists, if the bootstrap transaction fails, or if the key derivation fails
+    ///
+    /// ## Example
+    ///
+    /// ```rust,no_run
+    /// use minidb::{KeySource, MiniDB};
+    ///
+    /// // create a MiniDB
+    /// let db = MiniDB::builder("test.redb")
+    ///     // skipping table registering for convenience
+    ///     .key_source(KeySource::Password("secretpassword".to_string())) // if you want the database to be encrypted
+    ///     .build()
+    ///     .unwrap();
+    /// ```
     pub fn build(self) -> Result<MiniDB> {
         let db = Database::builder()
             .create(&self.path)
@@ -111,18 +255,51 @@ impl MiniDBBuilder {
     }
 }
 
+/// The key source
+///
+/// ## Variants
+///
+/// * `KeySource::Password(String)` - The key source is a password
+/// * `KeySource::PreDerived(ArgonKey)` - The key source is a pre-derived key (`[u8; 32]`)
+/// * `KeySource::ExternalKeyProvider(Box<dyn Fn() -> ArgonKey>)` - The key source is a function that returns a key (`[u8; 32]`)
 pub enum KeySource {
+    /// The key source is a password
     Password(String),
+
+    /// The key source is a pre-derived key (`[u8; 32]`)
     PreDerived(ArgonKey),
+
+    /// The key source is a function that returns a key (`[u8; 32]`)
     ExternalKeyProvider(Box<dyn Fn() -> ArgonKey>),
 }
 
+/// A MiniDB
+///
+/// This is a wrapper around [`redb::Database`], but also stores the [`XChaCha20Poly1305`] instance to handle the optional encryption
 pub struct MiniDB {
     db: Database,
     cipher: Option<XChaCha20Poly1305>,
 }
 
 impl MiniDB {
+    /// Creates a new [`MiniDBBuilder`]
+    ///
+    /// ## Arguments
+    ///
+    /// * `path` - The path to the database file
+    ///
+    /// ## Returns
+    ///
+    /// A new [`MiniDBBuilder`]
+    ///
+    /// ## Example
+    ///
+    /// ```rust,no_run
+    /// use minidb::MiniDB;
+    ///
+    /// // create a MiniDB builder with the file path
+    /// let db = MiniDB::builder("test.redb");
+    /// ```
     pub fn builder<P>(path: P) -> MiniDBBuilder
     where
         P: Into<PathBuf>,
@@ -132,6 +309,30 @@ impl MiniDB {
 
     // EMD OF BUILDERS
 
+    /// Retrieves all items from a table
+    ///
+    /// ## Arguments
+    ///
+    /// * `T` - The table model
+    ///
+    /// ## Returns
+    ///
+    /// A [`Result`] containing the vector of all items in the table `T`
+    ///
+    /// ## Errors
+    ///
+    /// Returns an error if the table is not found, if the table is not initialized, or if the decryption/deserialization fails
+    ///
+    /// ## Example
+    ///
+    /// ```rust,ignore
+    /// // retrieves all items from the table `Person`
+    /// let people = db.all::<Person>().unwrap();
+    ///
+    /// // or
+    ///
+    /// let people: Vec<Person> = db.all().unwrap();
+    /// ```
     pub fn all<T>(&self) -> Result<Vec<T>>
     where
         T: TableModel,
@@ -158,6 +359,23 @@ impl MiniDB {
         Ok(results)
     }
 
+    /// Creates the table if it doesn't exist
+    ///
+    /// Recommended to use [`MiniDBBuilder::table`] instead.
+    ///
+    /// ## Arguments
+    ///
+    /// * `T` - The table model
+    ///
+    /// ## Errors
+    ///
+    /// Returns an error if the table creation fails
+    ///
+    /// ## Example
+    ///
+    /// ```rust,ignore
+    /// db.create_table::<Person>().unwrap();
+    /// ```
     pub fn create_table<T>(&self) -> Result<()>
     where
         T: TableModel,
@@ -165,6 +383,9 @@ impl MiniDB {
         self.create_table_impl(T::TABLE)
     }
 
+    /// Creates the table if it doesn't exist
+    ///
+    /// Use [`MiniDB::create_table`] instead
     fn create_table_impl<K, V>(&self, table: TableDefinition<K, V>) -> Result<()>
     where
         K: redb::Key,
@@ -178,6 +399,24 @@ impl MiniDB {
         Ok(())
     }
 
+    /// Iterates over all items in a table and applies a function to each item
+    ///
+    /// ## Arguments
+    ///
+    /// * `T` - The table model
+    /// * `f` - The function to apply to each item
+    ///
+    /// ## Errors
+    ///
+    /// Returns an error if the table is not found, if the table is not initialized, or if the decryption/deserialization fails
+    ///
+    /// ## Example
+    ///
+    /// ```rust,ignore
+    /// db.for_each::<Person>(|person| {
+    ///     println!("Person name: {}", person.name);
+    /// }).unwrap();
+    /// ```
     pub fn for_each<T, F>(&self, mut f: F) -> Result<()>
     where
         T: TableModel,
@@ -203,6 +442,28 @@ impl MiniDB {
         Ok(())
     }
 
+    /// Inserts an item into a table
+    ///
+    /// ## Arguments
+    ///
+    /// * `T` - The table model
+    /// * `item` - The item to insert
+    ///
+    /// ## Errors
+    ///
+    /// Returns an error if the table is not found, if the table is not initialized, or if the encryption/serialization fails
+    ///
+    /// ## Example
+    ///
+    /// ```rust,ignore
+    /// let mut person = Person {
+    ///     id: String::new(), // ID must be new so it can be generated automatically
+    ///     name: "John Doe".to_string(),
+    ///     age: 31,
+    /// };
+    ///
+    /// db.insert(&mut person).unwrap();
+    /// ```
     pub fn insert<T>(&self, item: &mut T) -> Result<()>
     where
         T: TableModel,
@@ -231,6 +492,32 @@ impl MiniDB {
         Ok(())
     }
 
+    /// Inserts multiple items into a table
+    ///
+    /// ## Arguments
+    ///
+    /// * `T` - The table model
+    /// * `items` - The items to insert
+    ///
+    /// ## Errors
+    ///
+    /// Returns an error if the table is not found, if the table is not initialized, or if the encryption/serialization fails
+    ///
+    /// ## Example
+    ///
+    /// ```rust,ignore
+    /// let mut people = vec![
+    ///     Person {
+    ///         id: String::new(),
+    ///         name: "John Doe".to_string(),
+    ///         age: 31,
+    ///     },
+    ///
+    ///     // ...
+    /// ];
+    ///
+    /// db.insert_many(&mut people).unwrap();
+    /// ```
     pub fn insert_many<T>(&self, items: &mut [T]) -> Result<()>
     where
         T: TableModel,
@@ -262,6 +549,27 @@ impl MiniDB {
         Ok(())
     }
 
+    /// Checks if a table is empty
+    ///
+    /// ## Arguments
+    ///
+    /// * `T` - The table model
+    ///
+    /// ## Returns
+    ///
+    /// * `Ok(true)` if the table is empty
+    /// * `Ok(false)` if the table is not empty
+    ///
+    /// ## Errors
+    ///
+    /// Returns an error if the table is not found or couldn't be opened
+    ///
+    /// ## Example
+    ///
+    /// ```rust,ignore
+    /// let is_empty = db.is_empty::<Person>().unwrap();
+    /// println!("Is the table empty? {}", is_empty);
+    /// ```
     pub fn is_empty<T>(&self) -> Result<bool>
     where
         T: TableModel,
@@ -273,6 +581,36 @@ impl MiniDB {
             .context("failed to check if table is empty")
     }
 
+    /// Retrieves an item from a table
+    ///
+    /// ## Arguments
+    ///
+    /// * `T` - The table model
+    /// * `id` - The id of the item to retrieve
+    ///
+    /// ## Returns
+    ///
+    /// * `Ok(Some(item))` if the item was found
+    /// * `Ok(None)` if the item was not found
+    ///
+    /// ## Errors
+    ///
+    /// Returns an error if the table is not found, if the table is not initialized, or if the encryption/serialization fails
+    ///
+    /// ## Example
+    ///
+    /// ```rust,ignore
+    /// // retrieves an item from the table `Person`
+    /// let person = db.get::<Person>("person_id").unwrap().unwrap();
+    /// println!("Person name: {}", person.name);
+    ///
+    /// // or
+    ///
+    /// let person: Option<Person> = db.get("person_id").unwrap();
+    /// if let Some(person) = person {
+    ///     println!("Person name: {}", person.name);
+    /// }
+    /// ```
     pub fn get<T>(&self, id: &str) -> Result<Option<T>>
     where
         T: TableModel,
@@ -296,6 +634,7 @@ impl MiniDB {
         Ok(Some(item))
     }
 
+    /// Retrieves the salt from the meta table
     fn get_salt(&self) -> Result<String> {
         let value: Option<String> = self
             .get_meta(META_KEY_SALT)
@@ -311,6 +650,7 @@ impl MiniDB {
         }
     }
 
+    /// Retrieves an item from the meta table
     fn get_meta<T>(&self, key: &str) -> Result<Option<T>>
     where
         T: for<'a> Deserialize<'a>,
@@ -328,6 +668,31 @@ impl MiniDB {
         }
     }
 
+    /// Retrieves an item from the settings table
+    ///
+    /// ## Arguments
+    ///
+    /// * `T` - The table model
+    /// * `key` - The key of the item to retrieve
+    ///
+    /// ## Returns
+    ///
+    /// * `Ok(Some(item))` if the item was found
+    /// * `Ok(None)` if the item was not found
+    ///
+    /// ## Errors
+    ///
+    /// Returns an error if the table is not found, if the table is not initialized, or if the encryption/serialization fails
+    ///
+    /// ## Example
+    ///
+    /// ```rust,ignore
+    /// let setting = db.get_setting::<String>("key").unwrap().unwrap();
+    ///
+    /// // or
+    ///
+    /// let setting: Option<String> = db.get_setting("key").unwrap();
+    /// ```
     pub fn get_setting<T>(&self, key: &str) -> Result<Option<T>>
     where
         T: for<'a> Deserialize<'a>,
@@ -353,6 +718,7 @@ impl MiniDB {
         Ok(Some(item))
     }
 
+    /// Sets an item in the meta table
     fn set_meta<T>(&self, key: &str, value: &T) -> Result<()>
     where
         T: Serialize,
@@ -369,6 +735,23 @@ impl MiniDB {
         Ok(())
     }
 
+    /// Sets an item in the settings table
+    ///
+    /// ## Arguments
+    ///
+    /// * `T` - The table model
+    /// * `key` - The key of the item to set
+    /// * `value` - The value of the item to set
+    ///
+    /// ## Errors
+    ///
+    /// Returns an error if the table is not found, if the table is not initialized, or if the encryption/serialization fails
+    ///
+    /// ## Example
+    ///
+    /// ```rust,ignore
+    /// db.set_setting("key", "value").unwrap();
+    /// ```
     pub fn set_setting<T>(&self, key: &str, value: &T) -> Result<()>
     where
         T: Serialize,
@@ -394,6 +777,30 @@ impl MiniDB {
         Ok(())
     }
 
+    /// Updates an item in the table
+    ///
+    /// ## Arguments
+    ///
+    /// * `T` - The table model
+    /// * `item` - The item to update
+    ///
+    /// ## Errors
+    ///
+    /// Returns an error if the table is not found, if the table is not initialized, or if the encryption/serialization fails
+    ///
+    /// ## Example
+    ///
+    /// ```rust,ignore
+    /// let mut person = Person {
+    ///     id: "person_id".to_string(),
+    ///     name: "John Doe".to_string(),
+    ///     age: 31,
+    /// };
+    ///
+    /// person.age = 32;
+    ///
+    /// db.update(&person).unwrap();
+    /// ```
     pub fn update<T>(&self, item: &T) -> Result<()>
     where
         T: TableModel,
@@ -421,6 +828,36 @@ impl MiniDB {
         Ok(())
     }
 
+    /// Updates multiple items in the table
+    ///
+    /// ## Arguments
+    ///
+    /// * `T` - The table model
+    /// * `items` - The items to update
+    ///
+    /// ## Errors
+    ///
+    /// Returns an error if the table is not found, if the table is not initialized, or if the encryption/serialization fails
+    ///
+    /// ## Example
+    ///
+    /// ```rust,ignore
+    /// let mut people = vec![
+    ///     Person {
+    ///         id: "person_id".to_string(),
+    ///         name: "John Doe".to_string(),
+    ///         age: 31,
+    ///     },
+    ///
+    ///     // ...
+    /// ];
+    ///
+    /// for person in &mut people {
+    ///     person.age = 32;
+    /// }
+    ///
+    /// db.update_many(&mut people).unwrap();
+    /// ```
     pub fn update_many<T>(&self, items: &[T]) -> Result<()>
     where
         T: TableModel,
@@ -449,6 +886,26 @@ impl MiniDB {
         Ok(())
     }
 
+    /// Exports a table as a JSON string
+    ///
+    /// ## Arguments
+    ///
+    /// * `T` - The table model
+    /// * `pretty` - Whether to pretty print the JSON
+    ///
+    /// ## Returns
+    ///
+    /// * `String` - The JSON string
+    ///
+    /// ## Errors
+    ///
+    /// Returns an error if the table is not found, if the table is not initialized, or if the encryption/serialization fails
+    ///
+    /// ## Example
+    ///
+    /// ```rust,ignore
+    /// let json = db.export_table::<Person>(true).unwrap();
+    /// ```
     pub fn export_table<T>(&self, pretty: bool) -> Result<String>
     where
         T: TableModel,
@@ -463,6 +920,34 @@ impl MiniDB {
         Ok(json)
     }
 
+    /// Removes an item from the table
+    ///
+    /// ## Arguments
+    ///
+    /// * `T` - The table model
+    /// * `key` - The key of the item to remove
+    ///
+    /// ## Returns
+    ///
+    /// * `Ok(Some(item))` if the item was removed
+    /// * `Ok(None)` if the item was not found
+    ///
+    /// ## Errors
+    ///
+    /// Returns an error if the table is not found, if the table is not initialized, or if the encryption/serialization fails
+    ///
+    /// ## Example
+    ///
+    /// ```rust,ignore
+    /// let removed_person = db.remove::<Person>("person_id").unwrap().unwrap();
+    ///
+    /// // or
+    ///
+    /// let removed_person: Option<Person> = db.remove("person_id").unwrap();
+    /// if let Some(person) = removed_person {
+    ///     println!("Person name: {}", person.name);
+    /// }
+    /// ```
     pub fn remove<T>(&self, key: &str) -> Result<Option<T>>
     where
         T: TableModel,
@@ -491,6 +976,30 @@ impl MiniDB {
         Ok(result)
     }
 
+    /// Removes multiple items from the table
+    ///
+    /// ## Arguments
+    ///
+    /// * `T` - The table model
+    /// * `keys` - The keys of the items to remove
+    ///
+    /// ## Returns
+    ///
+    /// * `Vec<T>` - The items that were removed
+    ///
+    /// ## Errors
+    ///
+    /// Returns an error if the table is not found, if the table is not initialized, or if the encryption/serialization fails
+    ///
+    /// ## Example
+    ///
+    /// ```rust,ignore
+    /// let removed_people = db.remove_many::<Person>(&["person_id1", "person_id2", ...]).unwrap();
+    ///
+    /// // or
+    ///
+    /// let removed_people: Vec<Person> = db.remove_many(&["person_id1", "person_id2", ...]).unwrap();
+    /// ```
     pub fn remove_many<T>(&self, keys: &[&str]) -> Result<Vec<T>>
     where
         T: TableModel,
--- minidb/src/testing.rs
+++ minidb/src/testing.rs
@@ -1,4 +1,21 @@
 /// Run a function and print the time it took
+///
+/// ## Arguments
+///
+/// * `func` - The function to run
+///
+/// ## Example
+///
+/// ```rust,no_run
+/// use minidb::time_function;
+///
+/// fn add(a: u32, b: u32) -> u32 {
+///     a + b
+/// }
+///
+/// let c = time_function!(add(1, 2));
+/// println!("c: {}", c);
+/// ```
 #[macro_export]
 macro_rules! time_function {
     ($func:expr) => {{

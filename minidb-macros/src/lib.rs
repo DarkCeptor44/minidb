5fc1316f3d67c68ba821d29717b8e1736c684a73
DarkCeptor44 <16278483+DarkCeptor44@users.noreply.github.com>
2026-02-11 21:10:53 -0300

    workspace: chore: add license header to source files

--- minidb-macros/src/lib.rs
+++ minidb-macros/src/lib.rs
@@ -2,6 +2,17 @@
 //!
 //! proc-macro crates don't have generated rustdocs
 
+// Copyright (c) 2026, DarkCeptor44
+//
+// This file is licensed under the GNU Lesser General Public License
+// (either version 3 or, at your option, any later version).
+//
+// This software comes without any warranty, express or implied. See the
+// GNU Lesser General Public License for details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this software. If not, see <https://www.gnu.org/licenses/>.
+
 #![forbid(unsafe_code)]
 #![warn(clippy::pedantic, missing_debug_implementations, missing_docs)]
 
--- minidb/src/lib.rs
+++ minidb/src/lib.rs
@@ -1,5 +1,16 @@
 //! # MiniDB
 
+// Copyright (c) 2026, DarkCeptor44
+//
+// This file is licensed under the GNU Lesser General Public License
+// (either version 3 or, at your option, any later version).
+//
+// This software comes without any warranty, express or implied. See the
+// GNU Lesser General Public License for details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this software. If not, see <https://www.gnu.org/licenses/>.
+
 #![forbid(unsafe_code)]
 #![warn(clippy::pedantic, missing_docs)]
 #![allow(clippy::doc_markdown)]

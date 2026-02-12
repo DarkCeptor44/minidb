c1993c93549f8c73c94ed37e70c7f282931dfbbd
DarkCeptor44 <16278483+DarkCeptor44@users.noreply.github.com>
2026-02-11 22:45:17 -0300

    minidb: test: add a simple example

---
+++ minidb/examples/simple.rs
@@ -0,0 +1,57 @@
+use minidb::{MiniDB, Table};
+use serde::{Deserialize, Serialize};
+use tempfile::NamedTempFile;
+
+#[derive(Debug, Table, Serialize, Deserialize, PartialEq)]
+#[minidb(name = "people")]
+struct Person {
+    #[key]
+    id: String,
+    name: String,
+    age: u8,
+}
+
+fn main() {
+    let temp_file = NamedTempFile::new().unwrap();
+
+    // 1. Create a new database without encryption and only one table (Person)
+    let db = MiniDB::builder(temp_file.path())
+        .table::<Person>()
+        .build()
+        .unwrap();
+
+    // 2. Insert a new person
+    let mut person_to_insert = Person {
+        id: String::new(), // this must be empty because MiniDB will generate an ID automatically
+        name: "John Doe".to_string(),
+        age: 31,
+    };
+    db.insert(&mut person_to_insert).unwrap();
+    println!("Inserted person: {person_to_insert:?}");
+
+    // 3. Retrieve person by ID
+    let person_retrieved: Person = db.get(&person_to_insert.id).unwrap().unwrap();
+    assert_eq!(person_retrieved, person_to_insert);
+    println!("Successfully retrieved and verified person: {person_retrieved:?}");
+
+    // 4. Update person
+    person_to_insert.age += 1;
+    db.update(&person_to_insert).unwrap();
+    println!("Updated person: {person_to_insert:?}");
+
+    // 5. Retrieve updated person
+    let person_retrieved: Person = db.get(&person_to_insert.id).unwrap().unwrap();
+    assert_eq!(person_retrieved.age, 32);
+    println!("Successfully retrieved and verified updated person: {person_retrieved:?}");
+
+    // 6. Delete person
+    db.remove::<Person>(&person_to_insert.id).unwrap();
+    println!("Deleted person");
+
+    // 7. Verify person is deleted
+    let user_deleted: Option<Person> = db.get(&person_to_insert.id).unwrap();
+    assert!(user_deleted.is_none());
+    println!("Verified deletion");
+
+    println!("\nExample completed successfully");
+}
